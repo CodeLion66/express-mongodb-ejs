00b820a3ed7c4b84e52494f6084beeeea6e45cd0
chensx <191142809@qq.com>
2019-01-13 13:31:09 +0800

    连接服务器

--- app.js
+++ app.js
@@ -5,10 +5,12 @@ var app = express()
 
 app.set('views', './views/pages')
 app.set('view engine', 'ejs')
-app.listen(port)
+
 
 console.log('server start')
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')))
+app.locals.moment = require('moment')
+app.listen(port)
 
 // index page
 app.get('/', function(req, res) {
@@ -102,3 +104,4 @@ app.get('/admin/list', function(req, res) {
 		}]
 	})
 })
+https://github.com/liziqi7/movie.git
